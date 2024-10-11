4677985d6cee53ba6d58a7cbf8c06efc513148c1
DanCodes <dan@dancodes.online>
2024-10-11 08:30:35 +0100

    chore: cleanup navbar

--- components/Dashboard/Navbar.vue
+++ components/Dashboard/Navbar.vue
@@ -3,6 +3,10 @@ import type { Theme } from "daisyui";
 import { signOut } from "firebase/auth";
 import { themes } from "~/utils/theme";
 
+import IcBaselineAccountCircle from "~icons/ic/baseline-account-circle";
+import MaterialSymbolsNotifications from "~icons/material-symbols/notifications";
+import TablerChevronDown from "~icons/tabler/chevron-down";
+
 const auth = useFirebaseAuth();
 const user = useCurrentUser();
 
@@ -21,12 +25,15 @@ const signout = () => {
 <template>
     <div class="navbar bg-base-100 z-10 shadow-md ">
         <div class="flex-1">
-            <h1 class="text-2xl font-semibold ml-2">RedRabbit</h1>
+            <router-link :to="user ? '/dashboard' : '/'" class="text-lg font-bold text-primary">
+                <h1 class="text-2xl font-semibold ml-2">RedRabbit</h1>
+            </router-link>
         </div>
         <div class="flex-none">
             <div class="dropdown dropdown-end">
-                <div tabindex="0" role="button" class="btn btn-secondary m-1">
+                <div tabindex="0" role="button" class="btn min-h-0 h-10 btn-secondary m-1">
                     {{ theme }}
+                    <TablerChevronDown class="w-4 h-4 ml-1"></TablerChevronDown>
                 </div>
                 <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-[1] w-80 p-2 shadow">
                     <div class="flex flex-wrap gap-2">
@@ -40,24 +47,26 @@ const signout = () => {
                     </div>
                 </ul>
             </div>
-            <button v-if="user" class="btn btn-ghost ml-4  btn-circle">
-                <div class="indicator"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-6 w-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0">
-                        </path>
-                    </svg><span class="indicator-item badge badge-secondary badge-sm">15</span></div>
-            </button>
-            <div v-if="user" class="dropdown dropdown-end ml-4"><label tabindex="0"
-                    class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full"><img src="https://placeimg.com/80/80/people" alt="profile"></div>
+            <router-link to="/dashboard/notifications" v-if="user" class="btn btn-ghost ml-4 btn-circle">
+                <div class="indicator">
+                    <MaterialSymbolsNotifications class="text-2xl"></MaterialSymbolsNotifications>
+                    <span class="indicator-item badge badge-secondary badge-sm">15</span>
+                </div>
+            </router-link>
+            <div v-if="user" class="dropdown dropdown-end ml-4">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img v-if="user.photoURL" :src="user.photoURL" alt="profile">
+                        <IcBaselineAccountCircle v-else class="h-full w-full"></IcBaselineAccountCircle>
+                    </div>
                 </label>
                 <ul tabindex="0"
                     class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     <li class="justify-between">
-                        <a href="/app/settings-profile">Profile Settings<span class="badge">New</span></a>
+                        <router-link class="btn btn-ghost flex" to="/profile/settings">Profile Settings
+                            <span class="badge w-10 text-xs">New</span>
+                        </router-link>
                     </li>
-                    <li class=""><a href="/app/settings-billing">Bill History</a></li>
                     <div class="divider mt-0 mb-0"></div>
                     <li><button class="btn btn-ghost" @click="signout">Logout</button></li>
                 </ul>
