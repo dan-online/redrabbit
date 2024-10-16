8d5f000d15d1c30afd40e6f0541a27252b9eef95
Saifurrahmaan khan <sk902@kent.ac.uk>
2024-10-16 18:15:08 +0100

    fix(dashboard): make profile dropdown more responsive

--- components/Sidebar/ProfileDropdown.vue
+++ components/Sidebar/ProfileDropdown.vue
@@ -5,14 +5,15 @@ const props = defineProps<{
 </script>
 
 <template>
-  <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52">
-    <li class="justify-between">
-      <router-link class="btn btn-ghost flex" to="/profile/settings">
-        Profile Settings
-        <span class="badge w-10 text-xs">New</span>
+  <ul tabindex="0"
+    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-full min-w-[200px] max-w-xs">
+    <li>
+      <router-link class="btn btn-ghost justify-between w-full" to="/profile/settings">
+        <span>Profile Settings</span>
+        <span class="badge text-xs">New</span>
       </router-link>
     </li>
-    <div class="divider mt-0 mb-0"></div>
-    <li><button class="btn btn-ghost" @click="props.signout">Logout</button></li>
+    <div class="divider my-0"></div>
+    <li><button class="btn btn-ghost w-full" @click="props.signout">Logout</button></li>
   </ul>
 </template>
