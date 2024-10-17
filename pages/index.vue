25cff236dc445fe5fae7ab92d26b4bae5fefc1eb
Saifurrahmaan khan <syntexplayzmc@gmail.com>
2024-10-17 14:26:55 +0100

    feat(onboarding): initial commit for onboarding, refactoring code

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,27 @@
 <script setup>
+import { signOut } from "firebase/auth";
 import LogoNuxt from "~icons/logos/nuxt-icon";
 
-import { signOut } from "firebase/auth";
-
 const auth = useFirebaseAuth();
-
 const user = useCurrentUser();
+
+const handleSignOut = () => signOut(auth);
 </script>
+
 <template>
-  <div class="container py-4 mx-auto">
+  <div class="container mx-auto py-4">
     <LogoNuxt class="text-2xl" />
+    <h1 class="text-xl font-bold mt-4">Welcome to RedPanda</h1>
 
-    <h1>Welcome to RedPanda</h1>
-
-    {{ user?.email }} - {{ user?.displayName }}
-
-    <div class="mt-2">
-      <button class="btn btn-primary" v-if="user" @click="() => signOut(auth)">Sign Out</button>
-      <a class="btn btn-primary" v-else href="/login">Sign In</a>
+    <div v-if="user" class="mt-2">
+      <p>{{ user.email }} - {{ user.displayName }}</p>
+      <button @click="handleSignOut" class="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Sign Out
+      </button>
     </div>
+
+    <a v-else href="/login" class="mt-2 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      sign in ^-^
+    </a>
   </div>
 </template>
