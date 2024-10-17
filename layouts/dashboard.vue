483440b8326552ebb338eea4c75cbca880a9093d
Saifurrahmaan khan <syntexplayzmc@gmail.com>
2024-10-17 13:21:00 +0100

    refactor(navbar): move dashboard files to navbar for better organization

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,33 +1,34 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
+import Navbar from "~/components/NavBar/Navbar.vue";
 
 const open = useCookie("sidebar", {
-	default: () => false,
+    default: () => false,
 });
 
 const sidebarHeight = ref("100vh");
 
 onMounted(() => {
-	const updateSidebarHeight = () => {
-		const navbar = document.querySelector(".navbar") as HTMLElement;
-		if (navbar) {
-			sidebarHeight.value = `calc(100vh - ${navbar.clientHeight}px)`;
-		}
-	};
+    const updateSidebarHeight = () => {
+        const navbar = document.querySelector(".navbar") as HTMLElement;
+        if (navbar) {
+            sidebarHeight.value = `calc(100vh - ${navbar.clientHeight}px)`;
+        }
+    };
 
-	updateSidebarHeight();
-	window.addEventListener("resize", updateSidebarHeight);
+    updateSidebarHeight();
+    window.addEventListener("resize", updateSidebarHeight);
 
-	return () => {
-		window.removeEventListener("resize", updateSidebarHeight);
-	};
+    return () => {
+        window.removeEventListener("resize", updateSidebarHeight);
+    };
 });
 </script>
 
 <template>
     <div class="h-dvh">
         <div class="sticky top-0 z-10">
-            <DashboardNavbar />
+            <Navbar />
         </div>
         <div class="flex flex-row">
             <div class="sticky top-0 z-5 transition-position duration-300 ease-in-out"
