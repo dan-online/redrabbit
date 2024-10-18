6adfc7abcc86ddee403856b485dce76822cec34e
DanCodes <dan@dancodes.online>
2024-10-18 09:53:01 +0100

    feat: user server changes

---
+++ server/middleware/user.ts
@@ -0,0 +1,29 @@
+import { getAuth as getAdminAuth } from "firebase-admin/auth";
+import { ensureAdminApp } from "vuefire/server";
+
+export default defineEventHandler(async (event) => {
+	const runtimeConfig = useRuntimeConfig();
+	const token = getCookie(event, "__session");
+
+	if (!token) {
+		setResponseStatus(event, 403);
+
+		return {
+			error: "no token",
+		};
+	}
+
+	const adminApp = ensureAdminApp(
+		{
+			projectId: runtimeConfig.public.vuefire!.config!.projectId,
+		},
+		"session-verification",
+	);
+	const adminAuth = getAdminAuth(adminApp);
+
+	const verifiedIdToken = await adminAuth.verifySessionCookie(token, true);
+
+	event.context.user = verifiedIdToken;
+	event.context.adminApp = adminApp;
+	event.context.adminAuth = adminAuth;
+});
